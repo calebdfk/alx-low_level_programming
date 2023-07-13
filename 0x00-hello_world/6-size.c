065e794c3b01721ea40965dc86be46ce03ce6cb0
calebdfk <dkweteza@outlook.com>
2023-07-13 13:41:52 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,10 @@
  * Return: 0 (Success)
  */
 int main(void)
+
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("size of a long int: %zu byte(s)\n", sizeof(long));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(long long));
