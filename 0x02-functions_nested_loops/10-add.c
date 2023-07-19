e3ba9007e2c4d4735fa83e8d76da36d55c3b404b
calebdfk <dkweteza@outlook.com>
2023-07-18 17:54:43 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Add two calues
+ * @n1: The first number to compute
+ * @n2: The seconf number to compute
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+
+}
