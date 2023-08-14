b3b9a9fda55563a13196e9c09dd8d7e51978e274
calebdfk <dkweteza@outlook.com>
2023-08-14 11:22:48 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,12 +19,12 @@ struct dog
 /**
  * dog_l - typedef for struct dog
  */
-typedef struct dog dog_l;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_l *new_dog(char *name, float age, char *owner);
-void free_dog(dog_l *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
