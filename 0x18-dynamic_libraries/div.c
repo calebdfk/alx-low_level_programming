314663222c6b8c0a0059fa57ca9045883d66a4f2
calebdfk <dkweteza@outlook.com>
2023-12-11 16:40:03 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * dive - divide two intergers
+ * @a: first int
+ * @b: second int
+ * Return: addition
+ */
+int dive(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul - multiply two intergers
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
